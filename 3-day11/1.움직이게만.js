223ca6e2fcb102769063b293d2b35169e7bdcaf5
CJewon <122004059+sfdssf@users.noreply.github.com>
2023-10-25 12:57:37 +0900

    3-11

---
+++ 3-day11/1.움직이게만.js
@@ -0,0 +1,35 @@
+const contentsWrapper = 
+document.querySelector('.contents-wrapper');
+
+// contentsWrapper.style.transform =
+// `translateX(-300px)`
+
+
+const nextButton =
+document.querySelector('.next');
+const prevButton =
+document.querySelector('.prev');
+
+
+// nextButton을 누를때 -300
+
+
+let 슬라이더위치 = 0;
+
+nextButton.addEventListener('click', ()=>{
+    슬라이더위치 = 슬라이더위치 - 300
+    위치적용();
+})
+prevButton.addEventListener('click', ()=>{
+    슬라이더위치 = 슬라이더위치 + 300
+    위치적용();
+})
+
+function 위치적용() {
+    contentsWrapper.style.transform = `translateX(${슬라이더위치}px)`
+}
+
+
+
+// prevButton을 누를때 +300
+
